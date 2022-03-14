301631424088e7320b4a25ad407f6cedf17b50a6
SanyaMad <90633231+SanyaMad@users.noreply.github.com>
2022-03-14 13:55:07 +0300

    commit№2 14.03

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-let title;
-let screens;
-let screenPrice = 25;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "проект";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 25000;
+let rollback = Math.round(Math.random() * 50);
+let fullPrice = 175000;
+let adaptive = true;
+let arr = screens.split('');
 
-alert("Hello");
 
-console.log ('screenPrice', screenPrice);
+console.log ('title'+ " " + title);
+console.log ('Стоимость разработки сайта' + " " + fullPrice + " " + 'рублей');
+console.log ('adaptive'+ " " + adaptive);
+console.log ('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей');
+console.log ('rollback'+ " " + rollback);
+console.log (screens.length);
+console.log (screens.toLowerCase());
+console.log (arr);
+console.log ('Процент отката посреднику за работу ' + " " + Math.round((fullPrice * (rollback/100))));
+
+
+console.log (typeof title);
+console.log (typeof fullPrice);
+console.log (typeof adaptive);
+console.log (typeof screenPrice);
+console.log (typeof rollback);
